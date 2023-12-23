d760f6f6462d0d4cedb9dcb74029fd7e6df2f271
Carloshlc007 <carloshlc@hotmail.com>
2023-12-22 21:24:53 -0300

    audio

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -17,6 +17,12 @@ const state = {
     } 
 };
 
+function playSound(audioName){
+    let audio = new Audio(`./src/audio/${audioName}.m4a`);
+    audio.volume = 0.2;
+    audio.play();
+}
+
 function countDown(){
     state.values.currentTime--;
     state.view.time.textContent = state.values.currentTime;
@@ -53,13 +59,14 @@ function addListnerHitBox(){
                 state.values.result++;
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
+                playSound("hit");
            }
         });
     });
 }
 
 function init(){
-   //moveEnemy();
+//    moveEnemy();
    addListnerHitBox();
 }
 
